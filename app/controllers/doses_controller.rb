adc94dac04472e69d292be263d519d61e2b553ae
Basil JelalEldean <soli.geu@gmail.com>
2019-08-09 13:59:22 +0100

    Final app

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,7 +9,8 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(params[:cocktail_id])
     else
@@ -18,8 +19,10 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktails_path
+
+    redirect_to cocktail_path(cocktail)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Cosmopolitan")
+Cocktail.create(name: "Manhatan")
+Cocktail.create(name: "Ice tower")
+Cocktail.create(name: "El grando")
+
 Ingredient.destroy_all
 
 Ingredient.create(name: "lemon")
